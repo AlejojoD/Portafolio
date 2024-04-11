b0be0b2c68602d5a530df01dd98ef960edf02c4d
Alejandro Bautista <dalejandrobautista8@gmail.com>
2024-04-11 14:04:57 -0500

    tengo imagenes montadas en exp, pero pondré texto, queda mas elegante

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import '../src/links/about.css'
+import '../src/links/skills.css'
+import '../src/links/experience.css'
+
 import Wrapper from './wrapper';
 import About from './links/about'
+import Skills from './links/skills';
+import Experience from './links/experience';
 
 function App() {
   return (
     <div className="App">
       
-      <Wrapper>
-        
-      </Wrapper>
+      <Wrapper> </Wrapper>
       <About></About>
+      <Skills></Skills>
+      <Experience></Experience>
     </div>
   );
 }
--- src/links/experience.js
+++ src/links/experience.js
@@ -1,10 +1,38 @@
-function experience () {
+import React from 'react';
+import escritorio from '../sada.jpg';
+import '../links/experience.css';
+import usa from '../usa.jpg';
+import galeria from '../galeria.png';
+import agriculture from '../miraclee.png';
+import tictac from '../triki d.png';
+function Experience() {
     return ( 
-<div>
-    sadsadasdsadsadsadasd
-</div>
-
-     );
+        <div className="displayExp">
+            <button className="close-buttonAbout">×</button>
+            <h1>EXPERIENCE</h1>
+            <div className='experiencias1'>
+                <div className='USA'>
+                    <img src={usa} alt="JavaScript"></img>
+                    <p>Assistant Grower United States</p>    
+                </div>   
+                <div className='galeria'>
+                    <img src={galeria} alt="JavaScript"></img>
+                    <p>Photo Gallery Project</p>    
+                </div>
+            </div>
+            <div className='experiencias2'>
+                <div className='agriculture'>
+                    <img src={agriculture}alt="JavaScript"></img>
+                    <p>Agriculture Website</p>    
+                </div>   
+                <div className='tictac'>
+                    <img src={tictac} alt="JavaScript"></img>
+                    <p>Tic Tac Toe Game</p>    
+                </div>   
+            </div>  
+        </div>
+    );
 }
 
-export default experience ;
+
+export default Experience;
